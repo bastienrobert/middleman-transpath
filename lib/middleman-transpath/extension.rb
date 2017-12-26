e598152415bf957d80cd8daaa0e202080fdbeea9
Bastien <bastienrbt@protonmail.com>
2017-12-26 13:33:47 +0100

    New functionnalities, such great, simple use

--- lib/middleman-transpath/extension.rb
+++ lib/middleman-transpath/extension.rb
@@ -9,7 +9,7 @@ class Transpath < ::Middleman::Extension
   end
 
   helpers do
-    def path_translate(lang)
+    def link_translate_path(lang)
       begin
         current_page.data.dir.map { |dir| t("paths.#{dir}", locale: lang, :default => dir) + '/'}.join('')
       rescue
@@ -17,21 +17,40 @@ class Transpath < ::Middleman::Extension
       end
     end
 
-    def title_translate(lang)
-      t("paths.#{current_page.data.title}", locale: lang, :default => current_page.data.title)
+    def link_translate_slug(lang)
+      dynamic = current_page.data.dynamic
+      unless (defined?(dynamic.enable)).nil?
+        current_page
+        .locals[dynamic.data.parameterize.underscore.to_sym][lang.to_s]
+        .send(dynamic.slug.to_s)
+      else
+        t("paths.#{current_page.data.slug}", locale: lang, :default => current_page.data.slug)
+      end
+    end
+
+    def title_translate
+      dynamic = current_page.data.dynamic
+      unless (defined?(dynamic.enable)).nil?
+        current_page
+          .locals[dynamic.data.parameterize.underscore.to_sym][locale.to_s]
+          .send(dynamic.name.to_s)
+          .capitalize
+      else
+        t("titles.#{current_page.data.slug}", locale: lang, :default => current_page.data.slug).capitalize
+      end
     end
 
     def link_translate(lang)
       return link_to data.languages.send(lang.to_s),
       if lang === I18n.default_locale
         config[:host] + '/' +
-        path_translate(lang) +
-        title_translate(lang)
+        link_translate_path(lang) +
+        link_translate_slug(lang)
       else
         config[:host] + '/' +
         lang.to_s + '/' +
-        path_translate(lang) +
-        title_translate(lang)
+        link_translate_path(lang) +
+        link_translate_slug(lang)
       end
     end
   end
